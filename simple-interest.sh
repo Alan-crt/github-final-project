47c4a0995cad78f29424431eb1d635447004ddc6
Prasetttyo <53452951+Alan-crt@users.noreply.github.com>
2023-04-27 23:28:33 +0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,8 +5,8 @@
    # Do not use this in production. Sample purpose only.
    
    # Author: Upkar Lidder (IBM)
-   # Additional Authors: Alan-crt
-   # <your GitHub username>
+   # Additional Authors: 
+   # Alan-crt
    
    # Input:
    # p, principal amount
